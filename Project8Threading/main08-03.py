9b73f1759e2e7d09e0f32c2a1f3726167791da4b
Valentina <valentinaprodocimo@gmail.com>
2025-02-11 13:47:03 -0500

    Add files via upload

---
+++ Project8Threading/main08-03.py
@@ -0,0 +1,14 @@
+import threading
+
+def sum(name, value):
+    for i in range(0, value):
+        print(f"{name} : {i}")
+
+t1 = threading.Thread(target=sum, args=('Thread 1', 10))
+t2 = threading.Thread(target=sum, args=('Thread 2', 10))
+
+t1.start()
+t2.start()
+
+print("Main Thread")
+
