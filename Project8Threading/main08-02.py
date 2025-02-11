8282f4956845dd6001834254a296cc7b127ca2a9
Valentina <valentinaprodocimo@gmail.com>
2025-02-11 13:46:39 -0500

    Add files via upload

---
+++ Project8Threading/main08-02.py
@@ -0,0 +1,15 @@
+import threading
+import time
+
+def thread_1():
+    while True:
+        print("Thread 1 Operation")
+        time.sleep(1.0)
+
+t1 = threading.Thread(target=thread_1)
+t1.daemon = True
+t1.start()
+
+while True:
+    print("Main Operation")
+    time.sleep(2.0)
